a962641a451ee1f014624059203eb4a9fddeab94
Ahtesham Ul Haq(@hptouchpad519) <hptouchpad519@gmail.com>
2017-02-09 17:42:22 +0500

    Create multi_loop.cpp

---
+++ multi_loop.cpp
@@ -0,0 +1,20 @@
+//multi_loop.cpp
+// lists cubes from 1 to 10
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+int numb;     //define the variable
+for(numb=1; numb<=10 numb++)
+{
+cout << setw(4) << numb;     //dispaly the first column
+int cube = numb*numb*numb;               //calculate cube
+cout << setw(6) << cube <<endl;    //displaysecond column
+}
+return 0;
+
+}
